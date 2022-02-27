7ef0158aadf4b429ffb1f3556147ed68046c037f
jojoma8 <jojo_ma@hotmail.co.uk>
2022-02-27 00:24:57 +0000

    sliderOK

--- components/slider/Slider.js
+++ components/slider/Slider.js
@@ -61,28 +61,8 @@ export default function Slider() {
     return Math.abs(offset) * velocity;
   };
 
-  // const nextSlide = () => {
-  //   const activeSlide = document.querySelector(".slide.translate-x-0");
-  //   activeSlide.classList.remove("translate-x-0");
-  //   activeSlide.classList.add("-translate-x-full");
-
-  //   const nextSlide = activeSlide.nextElementSibling;
-  //   nextSlide.classList.remove("translate-x-full");
-  //   nextSlide.classList.add("translate-x-0");
-  // };
-
-  // const previousSlide = () => {
-  //   const activeSlide = document.querySelector(".slide.translate-x-0");
-  //   activeSlide.classList.remove("translate-x-0");
-  //   activeSlide.classList.add("translate-x-full");
-
-  //   const previousSlide = activeSlide.previousElementSibling;
-  //   previousSlide.classList.remove("-translate-x-full");
-  //   previousSlide.classList.add("translate-x-0");
-  // };
-
   return (
-    <div className="  mt-10   ">
+    <div className="  mt-10  overflow-x-hidden ">
       <div className="flex  items-center justify-center  ">
         <div className="flex-grow  h-80 max-w-5xl m-5">
           <div
@@ -150,50 +130,6 @@ export default function Slider() {
           ></div>
         ))}
       </div>
-      {/* <div className="flex items-center justify-center relative max-w-5xl m-5">
-        <Carousel
-          showStatus={false}
-          showThumbs={false}
-          autoPlay
-          infiniteLoop
-          interval={5000}
-        >
-          {sliderData.map((obj, index) => {
-            return (
-              <div
-                className={` bg-blue-10 rounded-3xl p-10 h-64 font-serif 
-            text-lg leading-loose italic text-gray-500 text-left
-            `}
-              >
-                {obj.title}
-              </div>
-            );
-          })}
-        </Carousel>
-      </div> */}
-      {/* <div className="relative mb-96">
-        <div className="absolute inset-0 w-screen h-48 bg-pink-500 text-white flex items-center justify-center text-5xl transition-all ease-in-out duration-1000 transform translate-x-0 slide">
-          Hello
-        </div>
-        <div className="absolute inset-0 w-screen h-48 bg-purple-500 text-white flex items-center justify-center text-5xl transition-all ease-in-out duration-1000 transform translate-x-full slide">
-          There
-        </div>
-        <div className="absolute inset-0 w-screen h-48 bg-teal-500 text-white flex items-center justify-center text-5xl transition-all ease-in-out duration-1000 transform translate-x-full slide">
-          Booya!
-        </div>
-        <div
-          onclick={() => nextSlide()}
-          className="cursor-pointer fixed top-40 right-0 bg-red-500 w-16 h-16 flex items-center justify-center text-black"
-        >
-          &#x276F;
-        </div>
-        <div
-          onclick={() => previousSlide()}
-          className="cursor-pointer fixed top-40 right-0 bg-red-200 w-16 h-16 mr-16 border-r border-gray-400 flex items-center justify-center text-black"
-        >
-          &#x276E;
-        </div>
-      </div> */}
     </div>
   );
 }
